8bd04552b1999ef858a995f692ca2a4fe85170c4
themmj <themmj@users.noreply.github.com>
2021-01-31 03:59:58 +0100

    Enable new tests and add namespace documentation

---
+++ src/namespaces.hpp
@@ -0,0 +1,19 @@
+#ifndef _NAMESPACES_H
+#define _NAMESPACES_H
+/** @file */
+
+/// TME
+namespace tme {
+
+    /// Essestials like logging, events etc.
+    namespace core {
+
+        /// Eventsystem including Dispatcher and base Event types.
+        namespace events {}
+
+    }
+
+}
+
+
+#endif
--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,6 +1,9 @@
 #include "gtest/gtest.h"
 
 #include "core/log_test.cpp"
+#include "core/loggable_test.cpp"
+#include "core/events/event_test.cpp"
+#include "core/events/dispatcher_test.cpp"
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
