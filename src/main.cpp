e39a8210b725e5819614d18644d06e93e429ad60
themmj <themmj@users.noreply.github.com>
2021-02-05 23:53:22 +0100

    Add example application

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,80 @@
+/** @file */
 #include <iostream>
+#include "namespaces.hpp"
 #include "core/log.hpp"
 
-#include <GLFW/glfw3.h>
+#include "core/events/dispatcher.hpp"
+#include "platform/glfw.hpp"
+#include "core/events/window.hpp"
+#include "core/events/key.hpp"
 
+namespace tme {
+    namespace core {
+        /// Example class to test event handling.
+        class App : public events::Dispatcher<App> {
+            bool m_running;
+
+            float red, green, blue;
+
+            bool handleWindowClose(events::WindowClose&) {
+                m_running = false;
+                return true;
+            }
+
+            bool handleKeyPress(events::KeyPressed& event) {
+                switch(event.getKey().getKeyCode()) {
+                    case TME_KEY_R:
+                        red += 0.1f;
+                        if (red > 1.0f)
+                            red = 0.0f;
+                        break;
+                    case TME_KEY_G:
+                        green += 0.1f;
+                        if (green > 1.0f)
+                            green = 0.0f;
+                        break;
+                    case TME_KEY_B:
+                        blue += 0.1f;
+                        if (blue > 1.0f)
+                            blue = 0.0f;
+                        break;
+                }
+                return true;
+            }
+
+            public:
+            App() : Dispatcher(this), m_running(true) {}
+
+            /// event callback function
+            void onEvent(events::Event& event) override {
+                TME_INFO("received event {}", event);
+                dispatchEvent<events::WindowClose>(event, &App::handleWindowClose);
+                dispatchEvent<events::KeyPressed>(event, &App::handleKeyPress);
+            }
+
+            /// run application
+            void run() {
+                auto window = Window::create({"This is a test", 640, 300, this});
+                while (m_running) {
+                    glClearColor( red, green, blue, 0.0f );
+                    glClear(GL_COLOR_BUFFER_BIT);
+                    window->onUpdate();
+                }
+                delete window;
+            }
+        };
+    }
+}
+
+/// Program entrypoint.
 int main() {
     tme::core::Log::init();
     TME_INFO("starting application");
 
-    GLFWwindow* window;
+    tme::core::App app;
+    app.run();
 
-    if (!glfwInit()) {
-        TME_CRITICAL("failed to initialize glfw");
-        return -1;
-    }
-
-    window = glfwCreateWindow(640, 480, "Test", NULL, NULL);
-    if (!window) {
-        TME_CRITICAL("failed to create window");
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-    glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
-
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-
-    TME_INFO("terminated glfw successfully");
+    TME_INFO("shutting down");
 
     return 0;
 }
