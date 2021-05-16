44fb8972314d4d4f65ffa81903f795272f780669
themmj <themmj@users.noreply.github.com>
2021-05-16 13:14:54 +0200

    Add simple shader

---
+++ res/vertex.glsl
@@ -0,0 +1,11 @@
+#version 330 core
+
+layout(location = 0) in vec2 position;
+
+void main()
+{
+    gl_Position.xy = position;
+    gl_Position.z = 0.0;
+    gl_Position.w = 1.0;
+};
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 /** @file */
 #include <iostream>
+#include "core/exceptions/validation.hpp"
 #include "imgui.h"
 #include "namespaces.hpp"
 #include "core/log.hpp"
@@ -14,6 +15,10 @@
 #include "core/window.hpp"
 #include "core/application.hpp"
 #include "core/graphics/shader.hpp"
+#include "core/graphics/vertex.hpp"
+#include "core/graphics/index.hpp"
+#include "core/graphics/buffer.hpp"
+#include "core/exceptions/input.hpp"
 
 #include "platform/context.hpp"
 
@@ -34,31 +39,88 @@ namespace tme {
 
         /// Example class to test event handling.
         class App : public WindowApplication {
-            float m_red;
+            float m_red, m_redIncr;
             Handle<Storage<graphics::Shader>> m_shadersHandle;
             Handle<Storage<graphics::Shader::Stage>> m_shaderStagesHandle;
+            Handle<Storage<graphics::VertexLayout>> m_vlHandle;
+            Handle<Storage<graphics::VertexBuffer>> m_vbHandle;
+            Handle<Storage<graphics::VertexArray>> m_vaHandle;
+            Handle<Storage<graphics::IndexBuffer>> m_ibHandle;
+
+            Handle<graphics::VertexArray> m_va;
+            Handle<graphics::Shader> m_shader;
+            Handle<graphics::IndexBuffer> m_ib;
 
             void update() override {
-                glClearColor(m_red, 0.5f, 0.5f, 0.0f );
-                glClear(GL_COLOR_BUFFER_BIT);
+                glClearColor(0.2f, 0.5f, 0.2f, 1.0f );
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+                m_shader->bind();
+                m_shader->setUniform4f("u_color", m_red, 0.0f, 0.0f, 0.5f);
+                m_va->bind();
+                m_ib->bind();
+
+                glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(m_ib->getPrimitiveCount()), GL_UNSIGNED_INT, nullptr);
+                m_red += m_redIncr;
+                if (m_red < 0.0f || m_red > 1.0f) {
+                    m_redIncr = -m_redIncr;
+                }
             }
 
+            struct Pos {
+                float x, y;
+            };
+
+            struct Index {
+                unsigned int first, second, third;
+            };
+
             public:
-            App(const std::string& name, float red) : WindowApplication(name), m_red(red) {
+            App(const std::string& name, float red) : WindowApplication(name), m_red(red), m_redIncr(0.005f) {
+                glEnable(GL_BLEND);
+                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
                 m_layers.push<DemoWindow>();
                 m_layers.push<layers::Imgui>();
                 m_shadersHandle = Storage<graphics::Shader>::localInstance();
                 m_shaderStagesHandle = Storage<graphics::Shader::Stage>::localInstance();
+                m_vlHandle = Storage<graphics::VertexLayout>::localInstance();
+                m_vbHandle = Storage<graphics::VertexBuffer>::localInstance();
+                m_vaHandle = Storage<graphics::VertexArray>::localInstance();
+                m_ibHandle = Storage<graphics::IndexBuffer>::localInstance();
 
                 TME_INFO("creating app {}", name);
+                auto layout = m_vlHandle->create();
+                layout->push<float>(2);
 
-                auto vertex = m_shaderStagesHandle->create(graphics::Shader::Stage::Type::Vertex, "/home/mmj/development/workspaces/cpp/tme/res/vertex.glsl");
-                auto fragment = m_shaderStagesHandle->create(graphics::Shader::Stage::Type::Fragment, "/home/mmj/development/workspaces/cpp/tme/res/fragment.glsl");
-                auto shader = m_shadersHandle->create(vertex, fragment);
-                shader->bind();
-                shader->setUniform1i("u_Texture", 10);
-                shader->setUniform1i("doesnt exist", 20);
-                shader->unbind();
+                auto vb = m_vbHandle->create(sizeof(Pos), 3);
+                Pos pos[3];
+                pos[0] = {-0.5f, -0.5f};
+                pos[1] = { 0.5f, -0.5f};
+                pos[2] = { 0.0f,  0.5f};
+                auto res = vb->add(3, &pos[0]);
+                if (res.offset == graphics::INVALID_OFFSET) {
+                    TME_WARN("could not add verticies to buffer");
+                }
+
+                m_va = m_vaHandle->create(vb, layout);
+
+                m_ib = m_ibHandle->create(3, sizeof(Index), 1);
+                Index index{0, 1, 2};
+                res = m_ib->add(1, &index);
+                if (res.offset == graphics::INVALID_OFFSET) {
+                    TME_WARN("could not add index to buffer");
+                }
+
+                try {
+                    auto vertex = m_shaderStagesHandle->create(graphics::Shader::Stage::Type::Vertex, "default", "/home/mmj/development/workspaces/cpp/tme/res/vertex.glsl");
+                    auto fragment = m_shaderStagesHandle->create(graphics::Shader::Stage::Type::Fragment, "default", "/home/mmj/development/workspaces/cpp/tme/res/fragment.glsl");
+                    m_shader = m_shadersHandle->create(vertex, fragment);
+
+                } catch(const exceptions::InvalidInput& e) {
+                    TME_ASSERT(false, "{}: {}", e.type(), e.what());
+                } catch(const exceptions::SyntaxError& e) {
+                    TME_ASSERT(false, "{}: {}", e.type(), e.what());
+                } CATCH_ALL
             }
             
             ~App() {
